3f2bbe6aa38e92e0666acd079d4d483e7403a015
André Leão <andre.sp.leao@gmail.com>
2015-11-28 12:23:24 -0200

    Refatoração da AplVenda.
    Nessa refatoração foram criadas novos métodos para deixar
    a função cadastroVenda mais legível.

--- src/main/java/br/edu/ifes/poo1/padariax/cln/cgt/AplVenda.java
+++ src/main/java/br/edu/ifes/poo1/padariax/cln/cgt/AplVenda.java
@@ -13,7 +13,6 @@ import br.edu.ifes.poo1.padariax.cln.cdp.Produto;
 import br.edu.ifes.poo1.padariax.cln.cdp.Venda;
 import br.edu.ifes.poo1.padariax.cln.util.Utilitario;
 import java.text.DateFormat;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -30,7 +29,7 @@ public class AplVenda {
     private Map mapaVenda;
     private DateFormat dateFormat;
     private Map mapaCliente;
-    private Map mapaProduto;   
+    private Map mapaProduto;
     private List<Venda> listaVenda;
     private List<Venda> listaVendaNaoFiado;
 
@@ -45,83 +44,127 @@ public class AplVenda {
     }
 
     /**
-     * Função responsável por transformar as linhas lidas do arquivo em uma Map
-     * de Venda para vendas Fiado.
+     * Função responsável por transformar as linhas lidas do arquivo em uma List
+     * de Venda. Ela cria um Map para vendas Fiado e um List para vendas não
+     * fiado. Quando o objeto scanner lê o último registro, os 2 tipos de vendas
+     * são unidos em uma única lista. 
      *
      * @param file - Caminho do arquivo
-     * @return listaCliente - Map de Vendas
-     * @throws ParseException
+     * @return listaCliente - List de Vendas     
      */
     public List<Venda> cadastroVenda(Arquivo file) {
         List<String> listaImportada = util.importar(file);
-        
 
         for (String linha : listaImportada) {
             Scanner sc = new Scanner(linha);
             sc.useDelimiter(";");
             String registro = sc.next();
-            int cliente = 0;
-            int ehData = registro.indexOf("/");
-            
-            if(ehData > 0){
-                Venda vendaNova = criaVenda(sc,registro);
-                listaVendaNaoFiado.add(vendaNova);
+            int cliente = 0;            
+
+            if (ehData(registro)) {
+                importaVendaNaoFiado(sc, registro);
             } else {
                 cliente = Integer.parseInt(registro);
+                importaVendaFiado(cliente, sc);
+            }            
+
+            if (ehUltimoRegistro(sc)) {
+                listaVenda = vendasCadastradas(mapaVenda, listaVendaNaoFiado);
             }
-            
+        }
 
-            if (mapaVenda.containsKey(cliente)) {
+        return listaVenda;
+    }
 
-                Venda vendaExistente = (Venda) mapaVenda.get(cliente);
+    /**
+     * Função responsável por verificar se o scanner encontra-se no último 
+     * registro do arquivo.
+     * @param sc
+     * @return 
+     */
+    private static boolean ehUltimoRegistro(Scanner sc) {
+        return !sc.hasNext();
+    }
+
+    /**
+     * Função responsável por verificar se o registro lido do arquivo, é uma 
+     * data.
+     * @param registro
+     * @return 
+     */
+    private boolean ehData(String registro) {               
+        return registro.indexOf("/") > 0;
+    }
+
+    /**
+     * Função responsável por adicionar a venda lida do arquivo na lista de vendas
+     * não fiado.
+     * @param sc
+     * @param registro 
+     */
+    private void importaVendaNaoFiado(Scanner sc, String registro) {
+        Venda vendaNova = criaVendaNaoFiado(sc, registro);
+        listaVendaNaoFiado.add(vendaNova);
+    }
+
+    /**
+     * Função responsável por adicionar a venda lida do arquivo no Map de vendas
+     * fiado.
+     * @param cliente
+     * @param sc
+     * @throws NumberFormatException 
+     */
+    private void importaVendaFiado(int cliente, Scanner sc) throws NumberFormatException {
+        if (mapaVenda.containsKey(cliente)) {
+
+            Venda vendaExistente = (Venda) mapaVenda.get(cliente);
 
 //                Pattern pattern = Pattern.compile("^.[0-9]+;[0-9]+;[0-9]+/[0-9]+/[0-9]+;");
 //                sc.skip(pattern);
-                sc.next();                
+            sc.next();
 
-                Produto produto = (Produto) mapaProduto.get(Integer.parseInt(sc.next()));
-                Item item = new Item(produto, Integer.parseInt(sc.next()));
+            Produto produto = (Produto) mapaProduto.get(Integer.parseInt(sc.next()));
+            Item item = new Item(produto, Integer.parseInt(sc.next()));
 
-                vendaExistente.getListaItens().add(item);
-                mapaVenda.put(vendaExistente.getCliente().getCodigo(), vendaExistente);
+            vendaExistente.getListaItens().add(item);
+            mapaVenda.put(vendaExistente.getCliente().getCodigo(), vendaExistente);
 
-            } else if (cliente != 0) {
-                Venda vendaNova = criaVenda(sc, cliente);
-                mapaVenda.put(vendaNova.getCliente().getCodigo(), vendaNova);
-            } 
-            
-            if(!sc.hasNext()){
-               listaVenda = vendasCadastradas(mapaVenda,listaVendaNaoFiado);
-            }
+        } else if (cliente != 0) {
+            Venda vendaNova = criaVendaFiado(sc, cliente);
+            mapaVenda.put(vendaNova.getCliente().getCodigo(), vendaNova);
         }
-        
-        return listaVenda;
     }
-    
-    
 
     /**
-     * Função responsável por retornar todas vendas importadas do arquivo.
+     * Função responsável por retornar uma lista com todas vendas importadas do
+     * arquivo.
+     *
      *
      * @param mapaVendasFiado
      * @param listaVendasNaoFiado
-     * @param file - Caminho do arquivo
-     * @return listaCliente - Map de Vendas
-     * @throws ParseException
+     * @return listaTodasVendas - Lista de Vendas
      */
-    public List<Venda> vendasCadastradas(Map mapaVendasFiado,List<Venda> listaVendasNaoFiado){
+    public List<Venda> vendasCadastradas(Map mapaVendasFiado, List<Venda> listaVendasNaoFiado) {
         List<Venda> listaTodasVendas = new ArrayList(mapaVendasFiado.values());
-        
-        for(Venda venda:listaVendasNaoFiado){
+
+        for (Venda venda : listaVendasNaoFiado) {
             listaTodasVendas.add(venda);
         }
         return listaTodasVendas;
     }
-    
-    private Venda criaVenda(Scanner sc, int clienteArquivo) {
+
+    /**
+     * Função responsável por criar um objeto Venda quando a venda lida do
+     * aqruivo, tiver o meio de pagamento Fiado(F).
+     *
+     * @param sc - Objeto Scanner
+     * @param clienteArquivo - Código do cliente lido do arquivo.
+     * @return VendaLocal - Objeto Venda
+     */
+    private Venda criaVendaFiado(Scanner sc, int clienteArquivo) {
         Venda VendaLocal = new Venda();
         List<Item> listaItem = new ArrayList();
-        
+
         try {
             Cliente cliente = (Cliente) mapaCliente.get(clienteArquivo);
             VendaLocal.setCliente(cliente);
@@ -139,11 +182,20 @@ public class AplVenda {
         return VendaLocal;
     }
 
-    private Venda criaVenda(Scanner sc,String dataVenda) {
+    /**
+     * Função responsável por criar um objeto Venda quando a venda lida do
+     * aqruivo, NÃO tiver meio de pagamento do tipo Fiado(F). Dessa maneira, o
+     * objeto cliente não deve ser preenchido.
+     *
+     * @param sc - Objeto Scanner
+     * @param dataVenda - Data da venda lida do arquivo.
+     * @return VendaLocal - Objeto Venda
+     */
+    private Venda criaVendaNaoFiado(Scanner sc, String dataVenda) {
         Venda VendaLocal = new Venda();
         List<Item> listaItem = new ArrayList();
-        
-        try {            
+
+        try {
             VendaLocal.setDataVenda(dateFormat.parse(dataVenda));
             Produto produto = (Produto) mapaProduto.get(Integer.parseInt(sc.next()));
             Item item = new Item(produto, Integer.parseInt(sc.next()));

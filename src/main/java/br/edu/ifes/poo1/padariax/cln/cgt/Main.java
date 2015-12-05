2fdea37b7e1817c84277c112e7097fa99b67c3b5
André Leão <andre.sp.leao@gmail.com>
2015-12-05 17:55:37 -0200

    Acerto na função menu.

--- src/main/java/br/edu/ifes/poo1/padariax/cln/cgt/Main.java
+++ src/main/java/br/edu/ifes/poo1/padariax/cln/cgt/Main.java
@@ -20,12 +20,20 @@ public class Main {
 
     private static Map mapaCliente;
     private static Map mapaFornecedor;
+    private static Map mapaProduto;
     private static Map mapaCompras;
     private static List<Venda> listaVendas;
     private static AplCliente aplCliente;
     private static AplFornecedor aplFornecedor;
+    private static AplProduto aplProduto;
     private static AplCompra aplCompra;
     private static AplVenda aplVenda;
+    private static AplRelatorio aplRelatorio;
+    private static Arquivo arquivoCliente;
+    private static Arquivo arquivoFornecedor;
+    private static Arquivo arquivoCompras;
+    private static Arquivo arquivoVendas;
+    private static Arquivo arquivoProdutos;
     private static Utilitario util;
 
     /**
@@ -35,12 +43,16 @@ public class Main {
     public static void main(String[] args) {
         aplCliente = new AplCliente();
         aplFornecedor = new AplFornecedor();
-        aplCompra = new AplCompra();
-        aplVenda = new AplVenda();
+        aplProduto = new AplProduto();
+        aplRelatorio = new AplRelatorio();
         util = new Utilitario();
 
         Scanner sc = new Scanner(System.in);
-        menu(sc);
+        try {
+            menu(sc);
+        } catch (Exception e) {
+        }
+        
 
     }
 
@@ -50,8 +62,9 @@ public class Main {
         System.out.println("Informe o nome dos arquivos de importação na seguinte ordem (Separados por vírgula): ");
         System.out.println("1 - Cadastro de Clientes");
         System.out.println("2 - Cadastro de Fornecedores");
-        System.out.println("3 - Registro de Compras");
-        System.out.println("4 - Registro de vendas");
+        System.out.println("3 - Cadastro de Produtos");
+        System.out.println("4 - Registro de Compras");
+        System.out.println("5 - Registro de vendas");
         String arquivos = sc.nextLine();
 
         Scanner linha = new Scanner(arquivos);
@@ -62,25 +75,33 @@ public class Main {
                 String opc = linha.next();
                 switch (opc) {
                     case "clientes.csv":
-                        Arquivo arquivoCliente = new Arquivo(caminho, "clientes.csv");
+                        arquivoCliente = new Arquivo(caminho, "clientes.csv");
                         System.out.println(arquivoCliente.toString());
                         mapaCliente = aplCliente.cadastroCliente(arquivoCliente);
                         break;
                     case "fornecedores.csv":
-                        Arquivo arquivoFornecedor = new Arquivo(caminho, "fornecedores.csv");
+                        arquivoFornecedor = new Arquivo(caminho, "fornecedores.csv");
                         mapaFornecedor = aplFornecedor.cadastroFornecedor(arquivoFornecedor);
                         break;
+                    case "produtos.csv":
+                        arquivoProdutos = new Arquivo(caminho, "produtos.csv");
+                        mapaProduto = aplProduto.cadastroProduto(arquivoProdutos);
+                        break;
                     case "compras.csv":
-                        Arquivo arquivoCompras = new Arquivo(caminho, "compras.csv");
+                        aplCompra = new AplCompra(aplFornecedor.cadastroFornecedor(arquivoFornecedor),
+                                aplProduto.cadastroProduto(arquivoProdutos));
+                        arquivoCompras = new Arquivo(caminho, "compras.csv");
                         mapaCompras = aplCompra.cadastroCompra(arquivoCompras);
                         break;
                     case "vendas.csv":
-                        Arquivo arquivoVendas = new Arquivo(caminho, "vendas.csv");
+                        aplVenda = new AplVenda(aplCliente.cadastroCliente(arquivoCliente),
+                                                aplProduto.cadastroProduto(arquivoProdutos));
+                        arquivoVendas = new Arquivo(caminho, "vendas.csv");
                         listaVendas = aplVenda.cadastroVenda(arquivoVendas);
                         break;
                     default:
-                        System.out.println("Arquivo \""+opc+"\" não existe! \n"
-                                + "As opções possíveis são: clientes.csv,fornecedores.csv,compras.csv,vendas.csv. \n"
+                        System.out.println("Arquivo \"" + opc + "\" não existe! \n"
+                                + "As opções possíveis são: clientes.csv,fornecedores.csv,produtos.csv,compras.csv,vendas.csv. \n"
                                 + "Favor verificar!");
                         throw new Exception("Erro de I/O");
                 }
